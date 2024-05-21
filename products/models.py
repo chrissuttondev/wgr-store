9edaa90f7876afded1d24ae72ed855c0067225e0
Chris Sutton <chrissutton1789@gmail.com>
2024-05-21 06:51:41 +0000

    Added ratings fields to products model and edited products.json to correct format

--- products/models.py
+++ products/models.py
@@ -13,9 +13,12 @@ class Products(models.Model):
     title = models.CharField(max_length=300)
     price = models.FloatField()
     description = models.CharField(max_length=500)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.URLField()
+    category = models.ForeignKey('category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    image_url = models.URLField()
     image = models.ImageField(null=True, blank=True)
+    rating_rate = models.FloatField(null=True, blank=True)
+    rating_count = models.IntegerField(null=True, blank=True,)
 
     def __str__(self):
-        return self.name
+        return self.title
