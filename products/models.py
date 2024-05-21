6390ccfce758c6b0d8bb948003cbced35fb7cb82
Chris Sutton <chrissutton1789@gmail.com>
2024-05-21 01:48:10 +0000

    Created Caterory and Products models. Attempted to loaddata from json fixtures.ISSUE- Data in incorrect format.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    title = models.CharField(max_length=300)
+    price = models.FloatField()
+    description = models.CharField(max_length=500)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.URLField()
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
