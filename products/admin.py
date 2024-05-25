2c0918d2c5c7ad4c89d0196f2945d2a6ae1d3598
Chris Sutton <chrissutton1789@gmail.com>
2024-05-25 10:12:29 +0000

    Rolled back producst migrations to zero. Re-written Product Model

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Products, Category
+from .models import Product, Rating
 
 # Register your models here.
-admin.site.register(Products)
-admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Rating)
--- products/models.py
+++ products/models.py
@@ -2,23 +2,30 @@ from django.db import models
 
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=100)
+# Rating Model
+class Rating(models.Model):
+    rate = models.FloatField()
+    count = models.ImageField()
 
     def __str__(self):
-        return self.name
+        return f" {self.rate}, ({self.count} ratings)"
 
 
-class Products(models.Model):
+# Product Model
+class Product(models.Model):
+    CATERGORY_CHOICES = [
+        ("men's clothing", "Men's Clothing"),
+        ("women's clothing", "Women's Clothing"),
+        ("jewelery", "Jewelery"),
+        ("electronics", "Electronics"),
+    ]
+
     title = models.CharField(max_length=300)
-    price = models.FloatField()
-    description = models.CharField(max_length=500)
-    category = models.ForeignKey('category', null=True, blank=True,
-                                 on_delete=models.SET_NULL)
-    image_url = models.URLField()
-    image = models.ImageField(null=True, blank=True)
-    rating_rate = models.FloatField(null=True, blank=True)
-    rating_count = models.IntegerField(null=True, blank=True,)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    category = models.CharField(max_length=50, choices=CATERGORY_CHOICES)
+    image_url = models.URLField(max_length=400)
+    image_local = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
