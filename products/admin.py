4e7afa6aacc0440b260e514c13310026837ca22e
Chris Sutton <chrissutton1789@gmail.com>
2024-05-25 18:47:20 +0000

    Loaded product data and customized admin

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'category',
+        'price',
+        'rate',
+        'image_local',
+    )
+
+    ordering = ('category',)
+
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
