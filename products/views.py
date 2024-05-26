39e8f58646ea4e5559266cd2ec5aaf21b3e59dd9
Chris Sutton <chrissutton1789@gmail.com>
2024-05-26 08:50:46 +0000

    Added product_info, template, views and url

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import render, get_object_or_404 # noqa
 from .models import Product
 
 
-# Create your views here.
+# Taken from the boutique ado walkthrough project.
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all products"""
 
     products = Product.objects.all()
 
@@ -14,4 +14,15 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-# Create your views here.
+
+# Taken from the boutique ado walkthrough project.
+def product_info(request, product_id):
+    """ A view to show individual products info"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_info.html', context)
