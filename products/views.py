24fb52fc34b9b891677723aa735798337582eb05
Chris Sutton <chrissutton1789@gmail.com>
2024-05-26 12:51:52 +0000

    Added form and view to enable search functionality. Functionality not working as expected yet

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404 # noqa
+from django.db.models import Q
 from .models import Product
+from products.forms import SearchForm
 
 
 # Taken from the boutique ado walkthrough project.
@@ -26,3 +28,28 @@ def product_info(request, product_id):
     }
 
     return render(request, 'products/product_info.html', context)
+
+
+print('Testing search')
+
+
+# Search view
+def product_search(request):
+    """ A view to show products based on user search queries"""
+    form = SearchForm()
+    query = request.GET.get('query')
+    products = Product.objects.all()
+
+    if query:
+        products = products.filter(
+            Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(category__icontains=query)
+        )
+    
+    context = {
+        'form': form,
+        'products': products,
+    }
+
+    return render(request, 'products_search.html', context)
