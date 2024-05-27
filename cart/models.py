a9bb31c58acbc612873acb2d029666ff99f886fe
Chris Sutton <chrissutton1789@gmail.com>
2024-05-27 16:34:22 +0000

    Rolled back all 'cart' app model migrations.Re-working code for the cart

--- cart/models.py
+++ cart/models.py
@@ -1,12 +1,2 @@
 from django.db import models
-from products.models import Product
 
-
-# Create your models here.
-class Cart(models.Model):
-    items = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=0)
-    date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.quantity} x {self.items.title}'
