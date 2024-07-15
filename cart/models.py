4e8b3e2283e793c6ada0166ac4e476314bb0ac05
Chris Sutton <chrissutton1789@gmail.com>
2024-07-15 08:42:33 +0000

    Created order and order items models in cart app

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
+import uuid
 
 
 class CartItem(models.Model):
@@ -14,3 +15,20 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} x {self.product.title}"
+
+
+# order model
+class order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    shipping_add = models.TextField(max_length=20, default='pending')
+    order_number = models.CharField(max_length=36, unique=True, editable=False, defaul=uuid.uuid4)
+
+
+# order item model
+class order_item(models.Model):
+    order = models.ForgienKey(Order, related_name="items", on_delete=modles.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
