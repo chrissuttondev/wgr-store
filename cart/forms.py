083fb936b687062cb8d84e67dc4c624b2efa81d0
Chris Sutton <chrissutton1789@gmail.com>
2024-07-15 14:18:01 +0000

    Added order_create, checkout and order confirmation views

--- cart/forms.py
+++ cart/forms.py
@@ -2,4 +2,10 @@ from django import forms
 
 
 class CheckoutForm(forms.Form):
-    shipping_add = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(max_length=12, required=True, label='Full Name')
+    shipping_add = forms.CharField(
+        widget=forms.Textarea,
+        required=True,
+        label='Shipping Address'
+        )
+    email_adress = forms.EmailField(required=False, label='Email Address')
--- cart/models.py
+++ cart/models.py
@@ -5,7 +5,10 @@ import uuid
 
 
 class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, null=True, blank=True,
+        on_delete=models.CASCADE
+        )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -22,7 +25,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
-    shipping_add = models.TextField(max_length=20, default='pending')
+    shipping_address = models.TextField(max_length=20, default='pending')
     order_number = models.CharField(
         max_length=36,
         unique=True, editable=False,
